21e026202351d14b6f1f6d60204187bc11cb6efc
Donggun Kim <donggunkim@hanwha.com>
2019-09-30 22:21:33 +0900

    backend 데이터 연동 및 axios 추가, config 파일 proxy 설정 완료

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 import { store } from './store/store'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   router,
